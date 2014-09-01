2eb3cdc7cee059ca3373ddced7c9624766091828
Nik Martin <nik.martin@gmail.com>
2014-08-31 22:13:45 -0500

    add files

---
+++ AssemblyInfo.vb
@@ -0,0 +1,68 @@
+'''#################################################
+''' ClipViewer.Net
+''' Allows you to view the HTML formatted text on the clipboard
+''' 2005 Nik Martin
+''' clipviewer@nik-martin.com
+
+''' This is the MIT License, with one addition:
+''' I want attribution in source code form, which means any code of mine you use,
+''' you must keep this entire license intact, including the next line:
+'''
+''' Nik Martin wrote the original version of this software.  
+''' http://www.nikmartin.com
+'''
+'''
+''' Copyright (c) 2005 Nik Martin <><
+'''
+''' Permission is hereby granted, free of charge, to any person obtaining a copy of 
+''' this software and associated documentation files (the "Software"), to deal in 
+''' the Software without restriction, including without limitation the rights to 
+''' use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
+''' the Software, and to permit persons to whom the Software is furnished to do so, 
+''' subject to the following conditions:
+'''
+''' The above copyright notice and this permission notice shall be included in all 
+''' copies or substantial portions of the Software.
+'''
+''' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+''' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+''' FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+''' COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+''' IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+''' CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+''' #################################################
+
+
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ClipViewer.exe")> 
+<Assembly: AssemblyDescription("Shows the HTML formatted text on the clipboard")> 
+<Assembly: AssemblyCompany("nik martin")> 
+<Assembly: AssemblyProduct("ClipViewer")> 
+<Assembly: AssemblyCopyright("2005")> 
+<Assembly: AssemblyTrademark("")> 
+<Assembly: CLSCompliant(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9FBF5A7E-9CCB-4994-A7F4-66757BDC2BCB")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("0.9.*")> 
---
+++ make.bat
@@ -0,0 +1,1 @@
+vbc /t:winexe /r:System.dll /optimize+ /main:frmClipView /out:ClipViewer.exe /r:System.Windows.Forms.dll,System.Xml.Dll,System.Drawing.dll, ClipViewer.vb AssemblyInfo.vb 
